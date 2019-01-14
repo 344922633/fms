68d071e4bec6809ba11a9b403d91392ad6dcc8ae
jiazhuangbushuai <2405260743@qq.com>
2019-01-14 09:12:08 +0800

    测试

--- fms-controller/src/main/java/com/fms/Test.java
+++ fms-controller/src/main/java/com/fms/Test.java
@@ -3,8 +3,8 @@ package com.fms;
 import java.io.File;
 
 public class Test {
-    public static void main(String[] args) { 
-        System.out.println("123.txt".substring("123.txt".lastIndexOf(".")));
+    public static void main(String[] args) {
+        System.out.println("124.txt".substring("123.txt".lastIndexOf(".")));
         String[] a = new String[]{"a", "b", "c", "d"};
         String[] b = new String[a.length - 1];
         System.arraycopy(a, 1, b, 0 , b.length);
